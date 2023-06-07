87778644132dea0fc462f4af3076bf22d7b26524
arkamaldeen <121355049+arkamaldeen@users.noreply.github.com>
2023-06-07 23:15:16 +0530

    #15 - adding the functionality to store the extracted data in a txt file (#16)

--- app-development/webapp for web scraping/app.py
+++ app-development/webapp for web scraping/app.py
@@ -11,12 +11,13 @@ import streamlit as st
 from PIL import Image
 import requests
 from bs4 import BeautifulSoup
-import json
+# import json
 import docx2txt
 # import textract
 from PyPDF2 import PdfFileReader
 import pdfplumber
-
+import os
+# from streamlit.runtime.state import SessionState
 
 # ---- LOAD ASSETS ----
 img_page_icon = Image.open("images/web_icon.jpeg")
@@ -24,14 +25,109 @@ img_page_icon = Image.open("images/web_icon.jpeg")
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="OdiaGenAI ", page_icon=img_page_icon, layout="wide")
 
+
 # Load CSS file
 def load_css(file_path):
     with open(file_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+
 # Load CSS file
 load_css('styles.css')
 
+
+# ----- FUNCTIONS ----
+# function to get the text from pdf using PyPDF2
+def read_pdf(file):
+    pdfReader = PdfFileReader(file)
+    count = pdfReader.numPages
+    # all_page_text = ""
+    # for i in range(count):
+    #     page = pdfReader.getPage(i)
+    #     all_page_text += page.extractText()
+    #
+    # return all_page_text
+    return count
+
+# function to run the enter button
+def run_function(url, documents):
+    data = ""
+    # Check if the user has provided a URL
+    if url:
+        try:
+            # Make a GET request to the URL and extract the text content
+            response = requests.get(url)
+            if response.status_code == 200:
+                text_content = response.text
+                # st.write(text_content)
+
+                soup = BeautifulSoup(text_content, 'html.parser')
+
+                # Extracting the header
+                h1_tags = soup.find_all("h1")
+                headline = [h1.get_text(strip=True) for h1 in h1_tags]
+                headline = '\n'.join(headline)
+
+                # Extracting the p p_tags
+                p_tags = soup.find_all("p")
+
+                # Extract the text content from each <p> tag
+                paragraphs = [p.get_text(strip=True) for p in p_tags]
+                paragraphs = '\n'.join(paragraphs)
+
+                data = data + (headline + '\n\n' + paragraphs)
+
+                # Display the extracted text content from url
+                st.text_area("Extracted Text", value=data, height=200)
+                return True, data
+
+        except requests.exceptions.RequestException as e:
+            st.error("Error: An exception occurred while fetching content from the URL.")
+            return False, data
+
+    # Check if the user has provided a document
+    elif documents is not None:
+        for document in documents:
+            document_details = {
+                "filename": document.name,
+                "filetype": document.type,
+                "filesize": document.size
+            }
+            st.write(document_details)
+
+            # Extract content from the txt file
+            if document.type == "text/plain":
+                # Read as bytes
+                data += str(document.read(), "utf-8")
+
+            # Extract content from the pdf file
+            elif document.type == "application/pdf":
+                # using PyPDF2
+                # data += read_pdf(document)
+
+                # using pdfplumber
+                try:
+                    with pdfplumber.open(document) as pdf:
+                        all_text = ""
+                        for page in pdf.pages:
+                            text = page.extract_text()
+                            all_text += text + "\n"
+                        data += all_text
+                except requests.exceptions.RequestException as e:
+                    st.write("None")
+
+            # Extract content from the docx file
+            elif document.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+                data += docx2txt.process(document)
+
+        # Display the extracted text content from file
+        st.write("attached")
+        st.text_area("Extracted Text", value=data, height=200)
+        return True, data
+
+    else:
+        st.error("Error: An error occurred while fetching content.")
+        return False, data
 
 
 # ---- HEADER SECTION ----
@@ -45,118 +141,65 @@ with st.container():
 
     st.markdown("<h1 class='title'>Odia Generative AI</h1>", unsafe_allow_html=True)
 
-
-
 # ---- BODY SECTION ----
+
 with st.container():
     st.subheader("Collecting monolingual data (Odia or any Indic Languages)")
 
-    # ----- FUNCTIONS ----
-    # function to get the text from pdf using PyPDF2
-    def read_pdf(file):
-        pdfReader = PdfFileReader(file)
-        count = pdfReader.numPages
-        # all_page_text = ""
-        # for i in range(count):
-        #     page = pdfReader.getPage(i)
-        #     all_page_text += page.extractText()
-        #
-        # return all_page_text
-        return count
-
-    # function to run the enter button
-    def run_function(url , documents):
-        news = ""
-        # Check if the user has provided a URL
-        if url:
-            try:
-                # Make a GET request to the URL and extract the text content
-                response = requests.get(url)
-                if response.status_code == 200:
-                    text_content = response.text
-
-                    soup = BeautifulSoup(text_content, 'html.parser')
-
-                    # Extracting the header
-                    # Extracting the script tag which includes the heading
-                    heading = soup.find('script', type='application/ld+json')
-
-                    # Extract the JSON data from the script tag
-                    json_data_heading = heading.string
-
-                    # Load the JSON data into a Python dictionary
-                    data = json.loads(json_data_heading)
-                    headline = data['headline']
-
-                    body = soup.find('div', class_='oi-article-lt')
-                    # Find all <p> tags within the div_tag
-                    p_tags = body.find_all('p')
-
-                    # Extract the text content from each <p> tag
-                    paragraphs = [p.get_text(strip=True) for p in p_tags]
-                    paragraphs = '\n'.join(paragraphs)
-
-                    news = news + (headline + '\n\n' + paragraphs)
-
-                    # Display the extracted text content from url
-                    st.text_area("Extracted Text", value=news, height=200)
-
-                else:
-                    st.error("Error: Unable to fetch content from the provided URL.")
-            except requests.exceptions.RequestException as e:
-                st.error("Error: An exception occurred while fetching content from the URL.")
-
-        # Check if the user has provided a document
-        elif documents is not None:
-            for document in documents:
-                document_details = {
-                    "filename":document.name,
-                    "filetype":document.type,
-                    "filesize":document.size
-                }
-                st.write(document_details)
-
-                # Extract content from the txt file
-                if document.type == "text/plain":
-                    # Read as bytes
-                    news += str(document.read(), "utf-8")
-
-                # Extract content from the pdf file
-                elif document.type == "application/pdf":
-                    # using PyPDF2
-                    # news += read_pdf(document)
-
-                    # using pdfplumber
-                    try:
-                        with pdfplumber.open(document) as pdf:
-                            all_text = ""
-                            for page in pdf.pages:
-                                text = page.extract_text()
-                                all_text += text + "\n"
-                            news += all_text
-                    except:
-                        st.write("None")
-
-                # Extract content from the docx file
-                else:
-                    news += docx2txt.process(document)
-
-            # Display the extracted text content from file
-            st.text_area("Extracted Text", value=news, height=200)
-        else:
-            st.error("Error: An error occurred while fetching content .")
-
-
-
+    # dividing the body section into 3 columns for url, attach button and enter button
     col1, col2, col3 = st.columns([0.6, 0.2, 0.2])
+    # url
     with col1:
         url = st.text_input(label='', placeholder="Enter URL")
-
+    # attached button
     with col2:
-        documents = st.file_uploader("", type=["png", "jpg", "jpeg", "pdf", "txt", "docx"], accept_multiple_files=True)
-
+        documents = st.file_uploader("", type=["pdf", "txt", "docx"], accept_multiple_files=True)
+        if not documents:
+            documents = None
+        else:
+            for doc in documents:
+                if doc.name.split(".")[-1].lower() not in ["pdf", "txt", "docx"]:
+                    st.error("Unsupported file: " + doc.name)
+    # enter button
     with col3:
-        b = st.button("Enter")
+        # Initialize state of button Enter
+        if "button_enter" not in st.session_state:
+            st.session_state.button_enter = False
 
-    if b:
-        run_function(url, documents)
+        if st.button("Enter"):
+            st.session_state.button_enter = True
+
+    extracted = False
+
+    if st.session_state.button_enter:
+        extracted, data = run_function(url, documents)
+
+        if extracted:
+            col1, col2 = st.columns([0.5, 0.5])
+
+            with col1:
+                saved_button = False
+                if st.button("Save",key="b_save"):
+                    file_name = "output.txt"
+
+                    # Define the folder path
+                    folder_path = "extracted data"
+
+                    # Create the folder if it doesn't exist
+                    os.makedirs(folder_path, exist_ok=True)
+
+                    # Define the file path
+                    file_path = os.path.join(folder_path, file_name)
+                    # Save string variable to file
+                    with open(file_path, "w", encoding="utf-8") as file:
+                        file.write(data)
+                    saved_button = True
+
+            with col2:
+                if st.button("Double click to Clear"):
+                    st.session_state.button_enter = False
+
+            if saved_button:
+                # Confirmation message
+                st.success(f"File saved as {file_name} in the current directory.")
+
