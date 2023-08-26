731d874eca32c64e47d1e552566d2190add3dc0e
Swateek Jena <swateek08@gmail.com>
2023-08-26 06:53:16 +0530

    #24 - running pre-commit hooks on file

--- app-development/webapp for web scraping/app.py
+++ app-development/webapp for web scraping/app.py
@@ -6,23 +6,26 @@
 # pip install pdfplumber
 # pip install justext
 
+import os
+
+# import json
+import docx2txt
 import justext
+import pdfplumber
+import requests
 import streamlit as st
+
+# import xml.dom.minidom
+from bs4 import BeautifulSoup
 from lxml import etree
-# import streamlit.components.v1 as components
 
 # File Processing pkgs
 from PIL import Image
-import requests
-# import xml.dom.minidom
-from bs4 import BeautifulSoup
-# import json
-import docx2txt
+
 # import textract
 from PyPDF2 import PdfFileReader
-import pdfplumber
-import os
 
+# import streamlit.components.v1 as components
 
 
 # ---- LOAD ASSETS ----
@@ -32,74 +35,85 @@ img_page_icon = Image.open("images/web_icon.jpeg")
 st.set_page_config(page_title="OdiaGenAI ", page_icon=img_page_icon, layout="wide")
 
 
-# Load CSS file
 def load_css(file_path):
+    """load_css
+
+    Load CSS file
+    """
     with open(file_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
 # Load CSS file
-load_css('styles2.css')
+load_css("styles2.css")
 
 
-# ----- FUNCTIONS ----
-# function to check whether the url is a sitemap or not
 def check_sitemap(url):
+    """check_sitemap
+
+    function to check whether the url is a sitemap or not
+    """
     # Check the URL's ending
-    if url.lower().endswith(('sitemap.xml', 'sitemap_index.xml', 'sitemap')):
+    if url.lower().endswith(("sitemap.xml", "sitemap_index.xml", "sitemap")):
         try:
             # Parse the content as XML
             response = requests.get(url)
             xml_content = etree.fromstring(response.content)
             # Check for sitemap-specific elements
-            if xml_content.tag == 'urlset' or xml_content.tag == 'sitemapindex':
+            if xml_content.tag == "urlset" or xml_content.tag == "sitemapindex":
                 return True
         except etree.XMLSyntaxError:
             pass
 
     # Additional conditions for identifying sitemaps
-    if 'sitemap' in url.lower():
+    if "sitemap" in url.lower():
         # Perform additional checks specific to the website's structure or naming conventions
         return True
 
     return False
 
 
-# function to get urls from the site map and extract those data
 def extract_urls_from_sitemaps(xml_url):
+    """extract_urls_from_sitemaps
+
+    function to get urls from the site map and extract those data
+    """
     # Make a GET request to the URL and extract the xml content
     response = requests.get(xml_url)
 
-    soup = BeautifulSoup(response.text, 'xml')
+    soup = BeautifulSoup(response.text, "xml")
     extracted_urls = []
 
     # check if the sitemap contains nested sitemaps
-    sitemap_tags = soup.find_all('sitemap')
+    sitemap_tags = soup.find_all("sitemap")
     if sitemap_tags:
         # Process nested sitemaps
         for sitemap_tag in sitemap_tags:
             print("sitemap_tags:" + sitemap_tag)
-            nested_url = sitemap_tag.find('loc').text
-            print('nested_url:', nested_url)
+            nested_url = sitemap_tag.find("loc").text
+            print("nested_url:", nested_url)
             nested_urls = extract_urls_from_sitemaps(nested_url)
             extracted_urls.extend(nested_urls)
     else:
         # Extract URLs from the current sitemap
-        loc_tags = soup.find_all('loc')
+        loc_tags = soup.find_all("loc")
         for loc_tag in loc_tags:
             # if loc_tag.parent.name != 'image':
             url = loc_tag.text
-            if url.endswith('.pdf') or url.endswith('.jpg') or url.endswith('.jpeg'):
+            if url.endswith(".pdf") or url.endswith(".jpg") or url.endswith(".jpeg"):
                 print(f"url skipped because it is a {url.split('.')[-1]}")
             else:
-                print('url:', url)
+                print("url:", url)
                 extracted_urls.append(url)
 
     return extracted_urls
 
 
-# function to check whether the entered url is valid
 def valid_url(url):
+    """valid_url
+
+    function to check whether the entered url is valid
+    """
     try:
         # Make a GET request to the URL and extract the text content
         response = requests.get(url)
@@ -107,44 +121,156 @@ def valid_url(url):
             return True
 
     except requests.exceptions.RequestException as e:
+        print(e)
         return False
 
 
-# function to create a custom stoplist for justext
 def custom_stoplist():
+    """custom_stoplist
+
+    function to create a custom stoplist for justext
+    """
     odia_stopwords = [
-        "ଏହି", "ଏକ", "ଏକାଉଣଟ", "ମୁଁ", "ମୋର", "ମୁଁ ନିଜେ", "ଆମେ", "ଆମର", "ଆମର", "ଆମେ ନିଜେ", "ତୁମେ", "ତୁମର", "ତୁମର",
-        "ନିଜେ", "ନିଜେ", "ସେ", "ତାଙ୍କୁ", "ତାଙ୍କର",
-        "ନିଜେ", "ସେ", "ତାଙ୍କୁ", "ତାଙ୍କର", "ନିଜେ", "ଏହା", "ଏହାର", "ନିଜେ |", "ସେମାନେ", "ସେଗୁଡିକ", "ସେମାନଙ୍କର",
-        "ସେମାନଙ୍କର", "ନିଜେ |", "କଣ", "ଯାହା", "କିଏ", "କାହାକୁ",
-        "ଏହା", "ତାହା", "ଏଗୁଡ଼ିକ", "ସେଗୁଡ଼ିକ", "ମୁଁ", "ହେଉଛି", "ହେଉଛି |", "ଥିଲା", "ଥିଲା |", "ହୁଅ", "ହୋଇସାରିଛି |", "ହେବା",
-        "ଅଛି", "ଅଛି", "ଥିଲା", "ଅଛି", "କର", "କରେ |",
-        "କରିଛନ୍ତି", "କରିବା", "ଏବଂ", "କିନ୍ତୁ", "ଯଦି", "କିମ୍ବା", "କାରଣ", "ଯେପରି", "ପର୍ଯ୍ୟନ୍ତ", "ଯେତେବେଳେ", "ର", "ପାଇଁ",
-        "ସହିତ", "ବିଷୟରେ", "ବିପକ୍ଷରେ", "ମଧ୍ୟରେ", "ଭିତରକୁ", "ମାଧ୍ୟମରେ",
-        "ସମୟରେ", "ପୂର୍ବରୁ", "ପରେ", "ଉପରେ", "ନିମ୍ନରେ |", "କୁ", "ଠାରୁ", "ଅପ୍", "ତଳକୁ", "ଭିତରେ", "ବାହାରେ", "ଉପରେ", "ବନ୍ଦ",
-        "ସମାପ୍ତ", "ତଳେ |", "ପୁନର୍ବାର", "ଆଗକୁ",
-        "ତାପରେ", "ଥରେ |", "ଏଠାରେ", "ସେଠାରେ", "କେବେ", "କେଉଁଠାରେ", "କିପରି", "ସମସ୍ତ", "ଉଭୟ", "ପ୍ରତ୍ୟେକ", "ଅଳ୍ପ", "ଅଧିକ",
-        "ଅଧିକାଂଶ", "ଅନ୍ୟ", "କେତେକ", "ଏହିପରି",
-        "ନୁହେଁ |", "କେବଳ", "ନିଜର", "ସମାନ", "ତେଣୁ", "ଅପେକ୍ଷା", "ମଧ୍ୟ", "ବହୁତ", "କରିପାରିବେ |", "ଇଚ୍ଛା", "କେବଳ",
-        "କରିବା ଉଚିତ", "ବର୍ତ୍ତମାନ"
+        "ଏହି",
+        "ଏକ",
+        "ଏକାଉଣଟ",
+        "ମୁଁ",
+        "ମୋର",
+        "ମୁଁ ନିଜେ",
+        "ଆମେ",
+        "ଆମର",
+        "ଆମର",
+        "ଆମେ ନିଜେ",
+        "ତୁମେ",
+        "ତୁମର",
+        "ତୁମର",
+        "ନିଜେ",
+        "ନିଜେ",
+        "ସେ",
+        "ତାଙ୍କୁ",
+        "ତାଙ୍କର",
+        "ନିଜେ",
+        "ସେ",
+        "ତାଙ୍କୁ",
+        "ତାଙ୍କର",
+        "ନିଜେ",
+        "ଏହା",
+        "ଏହାର",
+        "ନିଜେ |",
+        "ସେମାନେ",
+        "ସେଗୁଡିକ",
+        "ସେମାନଙ୍କର",
+        "ସେମାନଙ୍କର",
+        "ନିଜେ |",
+        "କଣ",
+        "ଯାହା",
+        "କିଏ",
+        "କାହାକୁ",
+        "ଏହା",
+        "ତାହା",
+        "ଏଗୁଡ଼ିକ",
+        "ସେଗୁଡ଼ିକ",
+        "ମୁଁ",
+        "ହେଉଛି",
+        "ହେଉଛି |",
+        "ଥିଲା",
+        "ଥିଲା |",
+        "ହୁଅ",
+        "ହୋଇସାରିଛି |",
+        "ହେବା",
+        "ଅଛି",
+        "ଅଛି",
+        "ଥିଲା",
+        "ଅଛି",
+        "କର",
+        "କରେ |",
+        "କରିଛନ୍ତି",
+        "କରିବା",
+        "ଏବଂ",
+        "କିନ୍ତୁ",
+        "ଯଦି",
+        "କିମ୍ବା",
+        "କାରଣ",
+        "ଯେପରି",
+        "ପର୍ଯ୍ୟନ୍ତ",
+        "ଯେତେବେଳେ",
+        "ର",
+        "ପାଇଁ",
+        "ସହିତ",
+        "ବିଷୟରେ",
+        "ବିପକ୍ଷରେ",
+        "ମଧ୍ୟରେ",
+        "ଭିତରକୁ",
+        "ମାଧ୍ୟମରେ",
+        "ସମୟରେ",
+        "ପୂର୍ବରୁ",
+        "ପରେ",
+        "ଉପରେ",
+        "ନିମ୍ନରେ |",
+        "କୁ",
+        "ଠାରୁ",
+        "ଅପ୍",
+        "ତଳକୁ",
+        "ଭିତରେ",
+        "ବାହାରେ",
+        "ଉପରେ",
+        "ବନ୍ଦ",
+        "ସମାପ୍ତ",
+        "ତଳେ |",
+        "ପୁନର୍ବାର",
+        "ଆଗକୁ",
+        "ତାପରେ",
+        "ଥରେ |",
+        "ଏଠାରେ",
+        "ସେଠାରେ",
+        "କେବେ",
+        "କେଉଁଠାରେ",
+        "କିପରି",
+        "ସମସ୍ତ",
+        "ଉଭୟ",
+        "ପ୍ରତ୍ୟେକ",
+        "ଅଳ୍ପ",
+        "ଅଧିକ",
+        "ଅଧିକାଂଶ",
+        "ଅନ୍ୟ",
+        "କେତେକ",
+        "ଏହିପରି",
+        "ନୁହେଁ |",
+        "କେବଳ",
+        "ନିଜର",
+        "ସମାନ",
+        "ତେଣୁ",
+        "ଅପେକ୍ଷା",
+        "ମଧ୍ୟ",
+        "ବହୁତ",
+        "କରିପାରିବେ |",
+        "ଇଚ୍ଛା",
+        "କେବଳ",
+        "କରିବା ଉଚିତ",
+        "ବର୍ତ୍ତମାନ",
     ]
     return frozenset(odia_stopwords)
 
 
-# function to extract data from url using justext
 def extract_data_from_url_(url):
+    """extract_data_from_url_
+
+    function to extract data from url using justext
+    """
     response = requests.get(url)
     response.raise_for_status()
     page = response.content
 
     data_url = ""
     para = ""
-    paragraphs = justext.justext(page, custom_stoplist(), 70, 140, 0.0, 0.02, 0.5, 150, False)
+    paragraphs = justext.justext(
+        page, custom_stoplist(), 70, 140, 0.0, 0.02, 0.5, 150, False
+    )
     for paragraph in paragraphs:
         if not paragraph.is_boilerplate:
-            para = para + '\n' + paragraph.text
+            para = para + "\n" + paragraph.text
 
-    data_url = ('\n\nFrom url:' + url + '\n' + para + '\n')
+    data_url = "\n\nFrom url:" + url + "\n" + para + "\n"
 
     return data_url
 
@@ -152,8 +278,11 @@ def extract_data_from_url_(url):
 sitemap_data = ""
 
 
-# function to get the text from pdf using PyPDF2
 def read_pdf(file):
+    """read_pdf
+
+    function to get the text from pdf using PyPDF2
+    """
     pdfReader = PdfFileReader(file)
     count = pdfReader.numPages
     # all_page_text = ""
@@ -165,8 +294,11 @@ def read_pdf(file):
     return count
 
 
-# function to run the enter button
 def run_function(url, documents):
+    """run_function
+
+    function to run the enter button
+    """
     data = ""
     # Check if the user has provided a URL
     if url:
@@ -178,14 +310,13 @@ def run_function(url, documents):
         else:
             return False, data
 
-
     # Check if the user has provided a document
     elif documents is not None:
         for document in documents:
             document_details = {
                 "filename": document.name,
                 "filetype": document.type,
-                "filesize": document.size
+                "filesize": document.size,
             }
             st.write(document_details)
 
@@ -208,10 +339,14 @@ def run_function(url, documents):
                             all_text += text + "\n"
                         data += all_text
                 except requests.exceptions.RequestException as e:
+                    print(e)
                     st.write("None")
 
             # Extract content from the docx file
-            elif document.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            elif (
+                document.type
+                == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+            ):
                 data += docx2txt.process(document)
 
         # Display the extracted text content from file
@@ -227,13 +362,19 @@ def run_function(url, documents):
 
 
 def main():
+    """main"""
     # ---- HEADER SECTION ----
     with st.container():
         st.subheader("Hi!! :wave:")
         st.write("##")
-        st.markdown("<h5 class='text'>OdiaGenAI is a collaborative initiative that conducts research on </h5>",
-                    unsafe_allow_html=True)
-        st.markdown("<h5>Generative AI and LLM for the Odia Language.</h5>", unsafe_allow_html=True)
+        st.markdown(
+            "<h5 class='text'>OdiaGenAI is a collaborative initiative that conducts research on </h5>",
+            unsafe_allow_html=True,
+        )
+        st.markdown(
+            "<h5>Generative AI and LLM for the Odia Language.</h5>",
+            unsafe_allow_html=True,
+        )
         # st.title("Odia Generative AI")
 
         st.markdown("<h1 class='title'>Odia Generative AI</h1>", unsafe_allow_html=True)
@@ -246,14 +387,14 @@ def main():
         col1, col2, col3 = st.columns([0.6, 0.2, 0.2])
         # url/xml
         with col1:
-
-            url_or_xml = st.text_input(label='', placeholder="Enter URL")
+            url_or_xml = st.text_input(label="", placeholder="Enter URL")
             is_a_sitemap = check_sitemap(url_or_xml)
 
         # attached files
         with col2:
-
-            documents = st.file_uploader("", type=["pdf", "txt", "docx"], accept_multiple_files=True)
+            documents = st.file_uploader(
+                "", type=["pdf", "txt", "docx"], accept_multiple_files=True
+            )
             if not documents:
                 documents = None
             else:
@@ -264,7 +405,7 @@ def main():
 
         # Initialize state of button Enter
         with col3:
-            st.write('##')
+            st.write("##")
             if "button_enter" not in st.session_state:
                 st.session_state.button_enter = False
 
@@ -283,17 +424,16 @@ def main():
                 if "Initial" not in st.session_state:
                     st.session_state.Initial = True
                 # check whether its the initial state
-                if st.session_state.Initial == True:
+                if st.session_state.Initial is True:
                     # print("\n\n\n\n1)Initial State", st.session_state.Initial, "\n\n\n\n\n")
                     xml = url_or_xml
                     st.write("It is a sitemap")
                     stored_sitemap_urls = extract_urls_from_sitemaps(xml)
-                    print('\nno. of urls: ', len(stored_sitemap_urls))
+                    print("\nno. of urls: ", len(stored_sitemap_urls))
 
                     if stored_sitemap_urls:
                         print(stored_sitemap_urls)
                         for sitemap_url in stored_sitemap_urls:
-
                             if valid_url(sitemap_url):
                                 print(sitemap_url)
                                 # using justext to extract data
@@ -306,13 +446,16 @@ def main():
                         # print("\n\n\nst.session.data ", st.session_state.sitemap_data)
                         # print("\n\n\n\nRUNNING \n\n\n\n")
                         st.session_state.Initial = False
-                        print("\n\n\n\n2)Initial State", st.session_state.Initial, "\n\n\n\n\n")
+                        print(
+                            "\n\n\n\n2)Initial State",
+                            st.session_state.Initial,
+                            "\n\n\n\n\n",
+                        )
                         st.session_state.extracted = True
                         # st.text_area("Extracted Text", value=st.session_state.sitemap_data, height=300)
 
                     else:
                         st.error("Error: Invalid sitemap.")
-
 
             else:
                 url = url_or_xml
@@ -320,7 +463,11 @@ def main():
 
             if st.session_state.extracted:
                 if is_a_sitemap:
-                    st.text_area("Extracted Text", value=st.session_state.sitemap_data, height=300)
+                    st.text_area(
+                        "Extracted Text",
+                        value=st.session_state.sitemap_data,
+                        height=300,
+                    )
                 col1, col2 = st.columns([0.5, 0.5])
 
                 with col1:
@@ -351,8 +498,8 @@ def main():
                         st.session_state.button_enter = False
                         st.session_state.Initial = True
                         st.session_state.extracted = False
-                        if 'sitemap_data' in st.session_state:
-                            del st.session_state['sitemap_data']
+                        if "sitemap_data" in st.session_state:
+                            del st.session_state["sitemap_data"]
                         st.session_state.button_enter = False
                         st.experimental_rerun()
 
@@ -366,4 +513,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
